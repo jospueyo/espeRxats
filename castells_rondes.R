94f7479201dcc6168eed45fb11d1dd11624be284
jospueyo <jospueyo@gmail.com>
2022-06-23 22:48:34 +0200

    create first files

---
+++ castells_rondes.R
@@ -0,0 +1,34 @@
+source("connection.R")
+
+get_table("diades_castells") %>% 
+  filter(!str_detect(castell, "\\dd[45]")) %>% 
+  filter(!str_detect(castell, "[pv]d")) %>%
+  filter(!str_detect(castell, "aco")) %>% 
+  filter(resultat == "d") %>% 
+  group_by(castell) %>% 
+  mutate(n = n()) %>% 
+  filter(n > 1) %>%
+  ungroup() %>% 
+  filter(ronda %in% c(2,3,4,5)) %>% 
+  mutate(ronda = factor(ronda -1)) %>% 
+  count(castell, ronda) %>% 
+  ggplot(aes(x=castell, y=n, fill=ronda))+
+  geom_col(position = position_fill())+
+  geom_text(aes(label=n, y=n), position = position_fill(vjust=0.5))+
+  scale_y_continuous(labels = scales::percent, expand = expansion(0))
+
+  
+# canvia castell i ronda per explorar casos que et generin interès
+
+explora_diada_on <- function(.castell, .ronda, .resultat){
+  doi <- get_table("diades_castells") %>% 
+    filter(castell == .castell & ronda == .ronda & resultat == .resultat) %>% 
+    pull(id_diada)
+  
+  get_table("diades_castells") %>% 
+    filter(id_diada %in% doi) %>% 
+    left_join(get_table("diades"), c("id_diada" = "id"))  
+}
+
+explora_diada_on("4d7", 4, "d")
+
---
+++ connection.R
@@ -0,0 +1,20 @@
+library(tidyverse)
+
+.password <- Sys.getenv("ESPERXATS_DB")
+
+if(.password == "") .password <- readline("Contrassenya a la base de dades: ")
+
+db <- DBI::dbConnect(RMySQL::MySQL(),
+                      host = "lhcp2008.webapps.net",
+                      user = "xj2y7bq5_pueyo",
+                      password = .password,
+                      dbname = "xj2y7bq5_wp375",
+                      port=3306)
+
+ 
+tables <- c("castells", "colles", "diades", "diades_castells", "diades_colles", "resultats")
+
+get_table <- function(.table, .db = db){
+  tbl(.db, .table) %>% 
+    as_tibble()
+}
